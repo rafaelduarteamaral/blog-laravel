c715ab14bca025c6fef24a1aff1416a5330e8a18
Gustavo <gustavotorres@gmail.com>
2020-12-04 11:25:14 -0300

    Adicionando arquivos

---
+++ resources/views/clientes.blade.php
@@ -0,0 +1,11 @@
+<html lang="pt-br">
+<head>
+    <title>YOOOOOO</title>
+</head>
+<body>
+<h2>Listando Clientes: </h2>
+<ul>
+    <li><a href="10">Guilherme</a></li>
+</ul>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,209 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class='headerProdutos'>
+        <div class='headerConteudo'>
+            <img>LOGO<img/>
+            <a>Home</a>
+            <a>My Books</a>
+            <select>
+                <option>Browse</option>
+                <option>Teste</option>
+                <option>Teste</option>
+                <option>Teste</option>
+            </select>
+            <select>
+                <option>Community</option>
+                <option>Teste</option>
+                <option>Teste</option>
+                <option>Teste</option>
+            </select>
+            <input type="text" placeholder="Buscar"/>
+            <button class="buttonHeader">Sign In</button>
+            <button class="buttonHeader">Join</button>
+        </div>
+    </div>
+
+    <div class="containerFundo">
+        <div class="containerContent">
+            <div class="sideContainerContent">
+                <span class="itemImg">IMAGEM</span>
+                <h3 class="itemBusca">Maquiagem</h3>
+                <h3 class="itemBusca">Batom</h3>
+                <h3 class="itemBusca">Maquiagem</h3>
+                <h3 class="itemBusca">Po</h3>
+                <h3 class="itemBusca">Maquiagem</h3>
+                <h3 class="itemBusca">Batom</h3>
+                <h3 class="itemBusca">Maquiagem</h3>
+                <h3 class="itemBusca">Po</h3>
+            </div>
+            <div class="listaContainerContent">
+                <div class="titleContent">
+                    <h1>Titulo do Menu</h1>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+                <div class="listaCardBox">
+                    <div class="imgCardBox">
+                    </div>
+                    <div class="lerMaisCardbox">
+                        <button class="buttonLerMais">read more</button>
+                    </div>
+                    <div class=avaliarCard>
+                        <h3 class="avaliarTexto">Rate this book</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="contentFooter">
+            <ul>
+                <li>COMPANY</li>
+                <li>About us</li>
+                <li>Careers</li>
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Interest Based Ads</li>
+                <li>Interest Based Ads</li>
+                <li>Ad Preferences</li>
+                <li>Help</li>
+            </ul>
+            <ul>
+                <li>WORK WITH US</li>
+                <li>Authors</li>
+                <li>Advertise</li>
+                <li>Authors & ads blog</li>
+                <li>API</li>
+            </ul>
+            <ul>
+                <li>CONNECT</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Instagram</li>
+                <li>Linkedin</li>
+                <li>teste1</li>
+            </ul>
+            <ul>
+                <li>WORK WITH US</li>
+                <li>Authors</li>
+                <li>Advertise</li>
+                <li>Authors & ads blog</li>
+                <li>API</li>
+            </ul>
+            <ul>
+                <li>CONNECT</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Instagram</li>
+                <li>Linkedin</li>
+                <li>teste1</li>
+            </ul>
+            <ul>
+                <li>WORK WITH US</li>
+                <li>Authors</li>
+                <li>Advertise</li>
+                <li>Authors & ads blog</li>
+                <li>API</li>
+            </ul>
+            <ul>
+                <li>CONNECT</li>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Instagram</li>
+                <li>Linkedin</li>
+                <li>teste1</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
