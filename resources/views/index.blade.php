76e15cfc6ba61126c17da12ab685e6748b22ccff
Gustavo <gustavotorres@gmail.com>
2020-12-05 13:27:12 -0300

    Home/Cabeçalho

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+    <title>Projeto</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <div class='headerProdutos'>
+        <div class='headerConteudo'>
+            <figure>
+                <img src="storage/logo.png" alt="Minha Figura">	
+            </figure>
+
+            <div class='headerEmail'>
+                <input type="email" placeholder="Email"/><br>
+                <input type="checkbox" name="Lembrar de mim">
+                <label for="Lembrar de mim">Lembrar de mim</label>
+            </div>
+            <div class="headerSenha">
+                <input type="password" placeholder="Senha"/></br>
+                <a href="resources/views/books.blade.php">Esqueceu?</a>
+            </div>
+            <div>
+                <button class="buttonHeader">Conectar</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="containerCadastro">
+            <div>
+
+            </div>
+            <div class="novaContas">
+                <h2>Novo Aqui? Cria sua conta gratuíta</h2>
+                <imput type="text" placegolder="Nome"> <br>
+                <imput type="email" placegolder="E-mail"> <br>
+                <imput type="password" placegolder="Senha"> <br>
+            </div>
+        </div>
+
+</body>
+</html>
